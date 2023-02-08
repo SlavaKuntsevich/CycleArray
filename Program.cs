f1f17ebe3e7ba36f7518866b1ced66a457755472
SlavaKuntsevich <mactep_chif@mail.ru>
2023-02-09 01:22:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Цикл
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            // ЦИКЛЫ
+        {   // Задача #1__________________________________________________________________
+            //for (int operandOne = 0; operandOne <= 99; operandOne++)
+            //{ if (operandOne % 2 != 0)
+            //    Console.WriteLine(operandOne);
+            //}
+            // Задача #2__________________________________________________________________
+            //  for (int operandOne = 5; operandOne > 0; operandOne--)
+            //{
+            //    Console.WriteLine(operandOne);
+            //}
+            // Задача #2 ver.2__________________________________________________________________
+            //int operandOne = 5;
+            //do
+            //{
+            //    Console.WriteLine(operandOne);
+            //    operandOne--;
+            //}while(operandOne > 0);
+            // Задача #3__________________________________________________________________
+            //Console.WriteLine("Введите любое целочисленное положительное число:");
+            //uint operandOne = Convert.ToUInt32(Console.ReadLine());
+            //uint operandTwo = 0;
+            //for (uint i = 0 ; i <= operandOne ; i++)
+            //{
+            //    operandTwo = operandTwo + i; 
+            //    Console.WriteLine(operandTwo);
+            //}
+            // Задача #4__________________________________________________________________
+            //int operandOne = 0;
+            //int operandTwo = 7;
+            //while (operandOne <=98) {
+            //    Console.WriteLine(operandOne);
+            //    operandOne = operandOne + 7;
+            //}
+            // Задача #5__________________________________________________________________
+            //int operandOne = 0;
+            //do
+            //{
+            //    Console.WriteLine(operandOne);
+            //    operandOne = operandOne - 5;
+            //} while (operandOne != -55);
+            //Задача #5 ver.2            
+            //for (int operandOne = 0; operandOne <= -50; operandOne--)
+            //{
+
+            //    Console.WriteLine(operandOne);
+            //    operandOne = operandOne - 5;
+
+            //}
+
+            // Задача #6__________________________________________________________________
+            //Console.WriteLine("Введите число от 10 до 20:");            
+            //for (int b = 0; b <= 20; b++)
+            //{
+            //     b = int.Parse(Console.ReadLine());
+            //    if (b < 10 || b >= 21) { break; }                
+            //    Console.WriteLine(Math.Sqrt(b));                
+            //}
+            // 
+            //kouodhwuihocshd;jco;i;isfho
+            
+            }
+
+
+    }
+}
